9b56bf37702e99abbbd37faa711d344928c58015
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-08 19:52:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 # app/main.py
+from app.routers import wrestler
 from fastapi import FastAPI
 from app.routers import auth
 
@@ -9,3 +11,4 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Dealfight Wrestling API"}
+app.include_router(wrestler.router, prefix="/wrestlers", tags=["Wrestlers"])
