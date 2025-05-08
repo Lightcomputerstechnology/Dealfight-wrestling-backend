7bdaf32f5c9e935650a5de5524a5bbe8c938e8f3
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-08 19:30:35 +0100

    Create routers/auth.py

---
+++ routers/auth.py
@@ -0,0 +1,23 @@
+# app/routers/auth.py
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.schemas.user import UserCreate, UserOut
+from app.models.user import User
+from app.core.database import SessionLocal
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/signup", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(username=user.username, email=user.email, hashed_password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
