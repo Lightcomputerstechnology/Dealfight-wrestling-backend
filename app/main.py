16355aca4157627c6539d1f7af3262cc2e4f6895
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 19:06:23 +0100

    Create app/main.py

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routers import auth, report
+
+app = FastAPI(title="Dealfight Wrestling API")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Dealfight Wrestling API"}
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(report.router, prefix="/report", tags=["Reports"])
