2ae6db3ed127f0ca40b90bd3f449e70658efa2ef
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 19:30:21 +0100

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,3 @@
 wrestlers = relationship("Wrestler", back_populates="owner", cascade="all, delete")
+matches_as_player1 = relationship("Match", foreign_keys='Match.player1_id')
+matches_as_player2 = relationship("Match", foreign_keys='Match.player2_id')
