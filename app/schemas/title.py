b7e561b1c6ba759949339e2fd1d7546c9d88a665
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 21:03:39 +0100

    Update title.py

--- app/schemas/title.py
+++ app/schemas/title.py
@@ -4,3 +4,7 @@ from typing import Literal
 class TitleAssign(BaseModel):
     type: Literal["World", "Tag Team", "Women"]
     holder_id: int
+
+    model_config = {
+        "from_attributes": True
+    }
