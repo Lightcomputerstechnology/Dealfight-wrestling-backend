b6ecb23b205b51218ec14ac896127cabd44a86f6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 19:33:08 +0100

    Update report.py

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -1,11 +1,7 @@
-app/schemas/report.py
-
 from pydantic import BaseModel
 from typing import Optional
 
 class ReportCreate(BaseModel):
     match_id: int
-    reporter_id: int
-    reason: str
+    issue: str
     details: Optional[str] = None
-
