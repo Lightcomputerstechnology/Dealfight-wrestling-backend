71b6ee95f1ad32ed1a94d9c80482b8eeae396eb4
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 19:20:26 +0100

    Update report.py

--- app/routers/report.py
+++ app/routers/report.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.report import ReportCreate
+from app.core.security import get_current_user
 
 router = APIRouter()
 
 @router.post("/")
-def create_report(report: ReportCreate):
-    return {"status": "Report received", "data": report}
-
+def create_report(report: ReportCreate, user=Depends(get_current_user)):
+    return {"status": "Report received", "report": report, "by": user}
