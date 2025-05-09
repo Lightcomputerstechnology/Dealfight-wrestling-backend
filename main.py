6201af8706debddf542db4f9be27f5ee233e11ef
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-09 14:46:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+# main.py
 
-# app/main.py
-from app.routers import admin
-from app.routers import report
-from app.routers import replay
-from app.routers import result
-from app.routers import title
-from app.routers import match
-from app.routers import wallet
-from app.routers import wrestler
+from routers import admin
+from routers import report
+from routers import replay
+from routers import result
+from routers import title
+from routers import match
+from routers import wallet
+from routers import wrestler
+from routers import auth
 from fastapi import FastAPI
-from app.routers import auth
 
 app = FastAPI(title="Dealfight Wrestling API")
 
@@ -18,18 +18,12 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Dealfight Wrestling API"}
+
 app.include_router(wrestler.router, prefix="/wrestlers", tags=["Wrestlers"])
-
 app.include_router(wallet.router, prefix="/wallet", tags=["Wallet"])
-
 app.include_router(match.router, prefix="/matches", tags=["Matches"])
-
 app.include_router(title.router, prefix="/titles", tags=["Titles"])
-
 app.include_router(result.router, prefix="/result", tags=["Results"])
-
 app.include_router(replay.router, prefix="/replays", tags=["Replay"])
-
 app.include_router(report.router, prefix="/report", tags=["Reports"])
-
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
