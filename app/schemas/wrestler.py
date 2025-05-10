85601e8ee1754b9c038cdd192b7508fd7a443b83
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-10 09:15:46 +0100

    Create app/schemas/wrestler.py

---
+++ app/schemas/wrestler.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+class WrestlerCreate(BaseModel):
+    name: str
+    gender: str
+    strength: int = 50
+    agility: int = 50
+    charisma: int = 50
+
+class WrestlerOut(BaseModel):
+    id: int
+    name: str
+    gender: str
+    strength: int
+    agility: int
+    charisma: int
+    owner_id: int
+
+    class Config:
+        from_attributes = True
