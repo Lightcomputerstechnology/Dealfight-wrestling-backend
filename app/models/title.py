9e5b1c19ba90e7add388af40e4882617d7d34c32
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-10 09:14:45 +0100

    Create app/models/title.py

---
+++ app/models/title.py
@@ -0,0 +1,19 @@
+import enum
+from datetime import datetime
+from sqlalchemy import Column, Integer, Enum, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class TitleType(str, enum.Enum):
+    World   = "World"
+    TagTeam = "Tag Team"
+    Women   = "Women"
+
+class TitleBelt(Base):
+    __tablename__ = "title_belts"
+    id         = Column(Integer, primary_key=True, index=True)
+    type       = Column(Enum(TitleType), nullable=False)
+    holder_id  = Column(Integer, ForeignKey("users.id"))
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    holder     = relationship("User", back_populates="titles")
