4432975e21b9411913f844d1c445e708b95f526b
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-10 09:10:45 +0100

    Create app/core/database.py

---
+++ app/core/database.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=sessionmaker)
+
+Base = declarative_base()
