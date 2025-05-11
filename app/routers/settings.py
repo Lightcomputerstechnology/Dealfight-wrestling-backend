99c204c4f915001067dd9e77651b42b3a32ea434
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 22:35:04 +0100

    Update settings.py

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -1,7 +1,8 @@
 ### app/routers/settings.py
 from fastapi import APIRouter, Depends
+from app.models.UserSetting import UserSetting
 from sqlalchemy.orm import Session
-from app.models.settings import UserSetting
+from app.models.settings import Setting
 from app.schemas.settings import SettingsUpdate
 from app.core.security import get_current_user
 from app.core.database import SessionLocal
