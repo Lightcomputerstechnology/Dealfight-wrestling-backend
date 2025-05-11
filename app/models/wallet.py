3f6297b1d724b8f23cf886beedf1071d981c76d6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 18:38:27 +0100

    Update wallet.py

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -4,6 +4,7 @@ from app.core.database import Base
 
 class Wallet(Base):
     __tablename__ = "wallets"
+
     id         = Column(Integer, primary_key=True, index=True)
     user_id    = Column(Integer, ForeignKey("users.id"), unique=True)
     coins      = Column(Integer, default=0)
