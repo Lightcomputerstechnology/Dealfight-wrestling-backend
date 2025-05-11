0358d930406710153c1d24eedd64219758bf23ac
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 07:02:06 +0100

    Update report.py

--- app/schemas/report.py
+++ app/schemas/report.py
@@ -1,8 +1,19 @@
+# app/schemas/report.py
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class ReportCreate(BaseModel):
-    reporter_id: int
     reported_id: int
     reason: str
     details: Optional[str] = None
+
+class ReportOut(BaseModel):
+    id: int
+    reported_id: int
+    reason: str
+    details: Optional[str]
+    timestamp: datetime
+
+    class Config:
+        from_attributes = True
