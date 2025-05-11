e28a294fd04aa05c25c2f94dc62b3219ea30a69b
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 19:23:15 +0100

    Update appeal.py

--- app/routers/appeal.py
+++ app/routers/appeal.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.core.security import get_current_user
 from app.core.database import SessionLocal
 from app.models.user import User
-from app.models.appeal import BanAppeal
+from app.models.appeal import Appeal
 from app.schemas.appeal import AppealCreate, AppealOut
 from typing import List
 
