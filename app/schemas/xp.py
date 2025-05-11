8bf6530440250c3c57ed2e11375e1258f26c8185
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 08:37:02 +0100

    Update xp.py

--- app/schemas/xp.py
+++ app/schemas/xp.py
@@ -1,6 +1,16 @@
 # app/schemas/xp.py
+
 from pydantic import BaseModel
+from datetime import datetime
 
 class XPUpdate(BaseModel):
-    match_id: int
-    won: bool  # True if user won, else False
+    won: bool  # Whether the user won the match or not
+
+class XPLogOut(BaseModel):
+    id: int
+    xp_gained: int
+    level: int
+    timestamp: datetime
+
+    class Config:
+        from_attributes = True
