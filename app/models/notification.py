efaa52ba44e34c2dbcc759dec1d1ea9032aea82e
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 18:39:21 +0100

    Update notification.py

--- app/models/notification.py
+++ app/models/notification.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 class Notification(Base):
     __tablename__ = "notifications"
+
     id         = Column(Integer, primary_key=True, index=True)
     user_id    = Column(Integer, ForeignKey("users.id"))
     message    = Column(String, nullable=False)
     timestamp  = Column(DateTime, default=datetime.utcnow)
 
-    user       = relationship("User", back_populates="notifications")
+    user = relationship("User", back_populates="notifications")
