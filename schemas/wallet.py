6f312dcea54c1c9612a16aaef0d3d25c9da526a1
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-05-11 07:13:25 +0100

    Create schemas/wallet.py

---
+++ schemas/wallet.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class WalletOut(BaseModel):
+    coins: int
+    diamonds: int
+
+    class Config:
+        from_attributes = True
+
+class WalletAdd(BaseModel):
+    type: str  # either "coins" or "diamonds"
+    amount: int
